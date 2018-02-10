1a9de6804fef9e6d3b6aee2ccfe96bc22e33cdb5
Arne Hassel <arne.hassel@bouvet.no>
2018-02-10 14:51:59 +0100

    Tester ut data fra UDP

--- udp-server.js
+++ udp-server.js
@@ -8,25 +8,57 @@ server.on('error', (err) => {
     server.close();
 });
 
+function filter(number, threshold) {
+    return number >= threshold || number <= threshold * -1 ? number : 0;
+}
+
+function unpack(msg, pos, threshold) {
+    return filter(jspack.Unpack('!f', msg, pos)[0], threshold);
+}
+
 server.on('message', (msg, rinfo) => {
     // var buf = new Buffer(msg);
     const data = {
         acc: {
-            x: jspack.Unpack('!f', msg, 0)[0],
-            y: jspack.Unpack('!f', msg, 4)[0],
-            z: jspack.Unpack('!f', msg, 8)[0]
+            x: unpack(msg, 0, 1),
+            y: unpack(msg, 4, 1),
+            z: unpack(msg, 8, 0.3)
+        },
+        gravity: {
+            x: unpack(msg, 12, 0.1),
+            y: unpack(msg, 16, 0.1),
+            z: unpack(msg, 20, 0.1)
         },
         rot: {
-            x: jspack.Unpack('!f', msg, 24)[0],
-            y: jspack.Unpack('!f', msg, 28)[0],
-            z: jspack.Unpack('!f', msg, 32)[0]
+            x: unpack(msg, 24, 0.1),
+            y: unpack(msg, 28, 0.1),
+            z: unpack(msg, 32, 0.1)
         }
     };
-    console.log(data);
-    // console.log(binary.parse(msg).word8s('x').vars);
-    // console.log(binary.parse(msg).word8s('y').vars);
-    // console.log(`server got: ${JSON.stringify(msg.data)}`);
-    // console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    // if (data.acc.x > 0) {
+    //     console.log('accelerating forward', data.acc.x);
+    // }
+    // if (data.acc.x < 0) {
+    //     console.log('stopping acceleration', data.acc.x);
+    // }
+    // if (data.acc.y > 0) {
+    //     console.log('accelerating', data.acc.y);
+    // }
+    // if (data.acc.y < 0) {
+    //     console.log('accelerating', data.acc.y);
+    // }
+    // if (data.acc.z > 0) {
+    //     console.log('going up', data.acc.z);
+    // }
+    // if (data.acc.z < 0) {
+    //     console.log('going down', data.acc.z);
+    // }
+    if (data.gravity.z > 0) {
+        console.log('going up', data.gravity.z);
+    }
+    if (data.gravity.z < 0) {
+        console.log('going down', data.gravity.z);
+    }
 });
 
 server.on('listening', () => {

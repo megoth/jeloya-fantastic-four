f9afaadd55c4ed08b313696663552e0a4e7b4dd3
Arne Hassel <arne.hassel@bouvet.no>
2018-02-10 14:26:46 +0100

    Gotten some good numbers

--- udp-server.js
+++ udp-server.js
@@ -1,5 +1,7 @@
 const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
+// const binary = require('binary');
+const jspack = require('jspack').jspack;
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
@@ -7,7 +9,23 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${JSON.stringify(msg)}`);
+    // var buf = new Buffer(msg);
+    const data = {
+        acc: {
+            x: jspack.Unpack('!f', msg, 0)[0],
+            y: jspack.Unpack('!f', msg, 4)[0],
+            z: jspack.Unpack('!f', msg, 8)[0]
+        },
+        rot: {
+            x: jspack.Unpack('!f', msg, 24)[0],
+            y: jspack.Unpack('!f', msg, 28)[0],
+            z: jspack.Unpack('!f', msg, 32)[0]
+        }
+    };
+    console.log(data);
+    // console.log(binary.parse(msg).word8s('x').vars);
+    // console.log(binary.parse(msg).word8s('y').vars);
+    // console.log(`server got: ${JSON.stringify(msg.data)}`);
     // console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
